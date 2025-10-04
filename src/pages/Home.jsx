25849960526fc4a5d52dc1e8b209d8b2845cb4f3
hamza-frontend <m.hamza.developer@gmail.com>
2025-10-04 16:52:50 +0500

    deployement bug testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import ContactForm from "@/components/ContactForm";
 import HeroSection from "@/components/HeroSection";
 import OurProcess from "@/components/OurProcess";
 import OurAchievements from "@/components/OurAchievements";
-import OurServices from "@/components/OurServices";
+import OurServices from "../components/OurServices";
 import RevenueSection from "@/components/Revenue";
 import TestimonialCarousel from "@/components/Testimonials";
 import TrustedBy from "@/components/TrustedBy";
